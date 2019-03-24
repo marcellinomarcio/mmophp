75295c1d203b5fc3ea5ae6ed9aaf8cee7f25e4e4
Marcelino Márcio <marcellinomarcio@gmail.com>
2019-03-24 17:04:18 -0300

    Adicionado uma linha de codigo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo("hello world everybody 456!");
 echo("Udemy");
 =======
 echo("hello world for everybody!");
-
+echo("Olá todos!");
 echo("Ola Hcode");
 >>>>>>> 7f52cb2249c4868532e3cea9c12d8f6533bedcd5
 
