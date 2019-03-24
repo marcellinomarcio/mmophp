1e2054534d7b5c326f3f3b5f30d7b8565841f88a
Marcelino Márcio <marcellinomarcio@gmail.com>
2019-03-24 19:57:59 -0300

    Este log

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ echo("hello world everybody 456!");
 echo("Udemy");
 =======
 echo("hello world for everybody!");
+echo("Olá mundo");
 echo("Olá todos!");
 echo("Ola Hcode");
 >>>>>>> 7f52cb2249c4868532e3cea9c12d8f6533bedcd5
